9246a4bdd92f7bd8894600ee3d18c2ed66e4eb63
Vitor Medezani <vitormedezani@gmail.com>
2024-11-13 12:46:29 -0300

    add a page's connetcion by include_once

--- index.php
+++ index.php
@@ -8,129 +8,151 @@
 </head>
 <body>
   
-<pre>
+  <div id="paginas">
+    <a aria-current="page" href="index.php"></a>
+  </div>
 
-<?php
+  <!-- <div align="center">
+    <form action="?page=verificar-jogador" method="post">
+    <label>Teste</label>
+    <input type="text" name="jogador">
+    <button type="submit">Enviar</button>
+  </form>
+  </div> -->
 
 
-$message = "<h1 align=center>Fazendo um jogo da velha!!</h1>";
-echo $message;
-//declarando principais variaveis/array
 
-$jogador = "X";
+  <pre align=center>
 
-$cpu = "O";
+  <?php
+  //declarando principais variaveis/array
 
-echo "<script>alert('Você escolheu jogar com " . strtoupper($jogador) . "');</script>";
 
-$v = array(
-  'a1' => "$jogador",
-  'a2' => " ",
-  'a3' => " ",
-  'b1' => " ",
-  'b2' => "$jogador",
-  'b3' => " ",
-  'c1' => " ",
-  'c2' => " ",
-  'c3' => "$jogador");
+  $jogador = "X";
 
-function ViewVelha($v1 = array()) {
-  echo "<h2 align=center>    A   B   C";
-  echo "\n   +---+---+---+";
-  echo "\n 1 | ".$v1['a1']." | ".$v1['b1']." | ".$v1['c1']." |";
-  echo "\n   |---+---+---|";
-  echo "\n 2 | ".$v1['a2']." | ".$v1['b2']." | ".$v1['c2']." |";
-  echo "\n   |---+---+---|";
-  echo "\n 3 | ".$v1['a3']." | ".$v1['b3']." | ".$v1['c3']." |";
-  echo "\n   +---+---+---+</h2>";
+  $cpu = "O";
 
-  return;
-}
+  $v = array(
+    'a1' => "$jogador",
+    'a2' => " ",
+    'a3' => " ",
+    'b1' => " ",
+    'b2' => "$jogador",
+    'b3' => " ",
+    'c1' => " ",
+    'c2' => " ",
+    'c3' => "$jogador");
 
-function VerificarVencedor($v1 = array()) {
+  function ViewVelha($v1 = array()) {
+    echo "<h2 align=center>    A   B   C";
+    echo "\n   +---+---+---+";
+    echo "\n 1 | ".$v1['a1']." | ".$v1['b1']." | ".$v1['c1']." |";
+    echo "\n   |---+---+---|";
+    echo "\n 2 | ".$v1['a2']." | ".$v1['b2']." | ".$v1['c2']." |";
+    echo "\n   |---+---+---|";
+    echo "\n 3 | ".$v1['a3']." | ".$v1['b3']." | ".$v1['c3']." |";
+    echo "\n   +---+---+---+</h2>";
 
-  switch (true) {
-    case ($v1['a1'] == $v1['a2'] and $v1['a2'] == $v1['a3']):
-      if (strtoupper($v1['a1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['a1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['b1'] == $v1['b2'] and $v1['b2'] == $v1['b3']):
-      if (strtoupper($v1['b1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['b1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['c1'] == $v1['c2'] and $v1['c2'] == $v1['c3']):
-      if (strtoupper($v1['c1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['c1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-      break;
-    case ($v1['a1'] == $v1['b1'] and $v1['b1'] == $v1['c1']):
-      if (strtoupper($v1['a1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['a1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['a2'] == $v1['b2'] and $v1['b2'] == $v1['c2']):
-      if (strtoupper($v1['a2']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['a2']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['a3'] == $v1['b3'] and $v1['b3'] == $v1['c3']):
-      if (strtoupper($v1['a3']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['a3']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['a1'] == $v1['b2'] and $v1['b2'] == $v1['c3']):
-      if (strtoupper($v1['a1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['a1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1['c1'] == $v1['b2'] and $v1['b2'] == $v1['a3']):
-      if (strtoupper($v1['c1']) == "X") {
-        echo "<script>alert('Você ganhou!!')</script>";
-        break;
-      } elseif (strtoupper($v1['c1']) == "O") {
-        echo "<script>alert('Você perdeu!!')</script>";
-        break;
-      }
-    case ($v1 != " "):
-      echo "<script>alert('Empate!!')</script>";
-      break;
-    
+    return;
   }
-}
 
-//echo ViewVelha($v['a1'], $v['a2'], $v['a3'], $v['b1'], $v['b2'], $v['b3'], $v['c1'], $v['c2'], $v['c3']);
-call_user_func('ViewVelha', $v);
-call_user_func('VerificarVencedor', $v);
+  function VerificarVencedor($v1 = array()) {
+
+    switch (true) {
+      case ($v1['a1'] == $v1['a2'] and $v1['a2'] == $v1['a3']):
+        if (strtoupper($v1['a1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['a1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['b1'] == $v1['b2'] and $v1['b2'] == $v1['b3']):
+        if (strtoupper($v1['b1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['b1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['c1'] == $v1['c2'] and $v1['c2'] == $v1['c3']):
+        if (strtoupper($v1['c1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['c1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+        break;
+      case ($v1['a1'] == $v1['b1'] and $v1['b1'] == $v1['c1']):
+        if (strtoupper($v1['a1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['a1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['a2'] == $v1['b2'] and $v1['b2'] == $v1['c2']):
+        if (strtoupper($v1['a2']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['a2']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['a3'] == $v1['b3'] and $v1['b3'] == $v1['c3']):
+        if (strtoupper($v1['a3']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['a3']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['a1'] == $v1['b2'] and $v1['b2'] == $v1['c3']):
+        if (strtoupper($v1['a1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['a1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1['c1'] == $v1['b2'] and $v1['b2'] == $v1['a3']):
+        if (strtoupper($v1['c1']) == "X") {
+          echo "<script>alert('Você ganhou!!')</script>";
+          break;
+        } elseif (strtoupper($v1['c1']) == "O") {
+          echo "<script>alert('Você perdeu!!')</script>";
+          break;
+        }
+      case ($v1 != " "):
+        echo "<script>alert('Empate!!')</script>";
+        break;
+      
+    }
+  }
+
+  //echo ViewVelha($v['a1'], $v['a2'], $v['a3'], $v['b1'], $v['b2'], $v['b3'], $v['c1'], $v['c2'], $v['c3']);
+  call_user_func('ViewVelha', $v);
+  call_user_func('VerificarVencedor', $v);
+
+
+  switch (@$_REQUEST["page"]) {
+    case 'verificar-jogador':
+      include_once("verificar-jogador.php");
+      break;
+    case 'velha':
+      include_once("velha.php");
+      break;
+    default:
+      echo "<div align=\"center\"><form action=\"?page=verificar-jogador\" method=\"post\"><h1>Como deseja jogar?</h1><br><button type=\"submit\" name=\"jogador\" value=\"X\">X</button><button type=\"submit\" name=\"jogador\" value=\"O\">O</button></form></div>";
+      break;
+  }
+  
 
 
 
-
-?>
-</pre>
+  ?>
+  </pre>
 </body>
 </html>
 
